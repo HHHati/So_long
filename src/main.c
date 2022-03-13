453d8799392cac0e2ffef2bc8e92e2d844cb7296
Basile De leeuw <bade-lee@fu-r7-p3.s19.be>
2022-03-13 15:29:50 +0100

    1.5

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: bade-lee <bade-lee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/13 14:37:47 by bade-lee          #+#    #+#             */
-/*   Updated: 2022/03/13 15:08:04 by bade-lee         ###   ########.fr       */
+/*   Updated: 2022/03/13 15:27:39 by bade-lee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,5 @@ int	main(void)
 	title = "test";
 	instance = mlx_init();
 	window = mlx_new_window(instance, sizex, sizey, title);
+	mlx_loop(instance);
 }
