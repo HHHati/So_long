292b788c4c0a2c8c5905a1cb87c4ecea5872aca2
Portable.Basile <basile_deleeuw@yahoo.be>
2022-04-27 14:27:54 +0200

    4.3

--- libft/include/get_next_line.h
+++ libft/include/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: Basile19 <Basile19@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 17:17:24 by bade-lee          #+#    #+#             */
-/*   Updated: 2022/04/27 14:25:40 by Basile19         ###   ########.fr       */
+/*   Updated: 2022/04/27 14:27:43 by Basile19         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,10 +34,7 @@ char	*ft_update(char *str);
 
 //          ----------========== {   GNL FCTS   } ==========----------
 
-static char	*ft_strchr_gnl(char *s, int c);
-static char	*ft_strjoin_gnl(char *str, char *buff);
 char	*ft_read(int fd, char *str);
-static char	*output_line(char **str, int fd);
 char	*get_next_line(int fd);
 
 #endif
